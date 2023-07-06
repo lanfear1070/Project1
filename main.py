79723e82397679a076f2b99a4d740a37024ebfe5
OhWanho <puppy1070@gmail.com>
2023-07-06 17:27:51 +0900

    제곱

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def add_func(n1,n2):
     return n1+n2
 def sub_func(n1,n2):
     return n1-n2
+<<<<<<< HEAD
 def mul_func(n1,n2):
     return n1*n2
 def div_func(n1,n2):
     return n1/n2
+def com_func(n1,n2):
+    return n1**n2
+=======
+
+>>>>>>> parent of 6e243f6 (곱하기 추가)
 
 
 ##전역변수부
@@ -19,6 +25,7 @@ print(num1,'+',num2,'=',result)
 
 result=sub_func(num1,num2)
 print(num1,'-',num2,'=',result)
+<<<<<<< HEAD
 
 result=mul_func(num1,num2)
 print(num1,'-',num2,'=',result)
@@ -27,4 +34,9 @@ result=div_func(num1,num2)
 print(num1,'-',num2,'=',result)
 
 
+result=com_func(num1,num2)
+print(num1,'^',num2,'=',result)
 
+
+=======
+>>>>>>> parent of 6e243f6 (곱하기 추가)
