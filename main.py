a909074d2effd38a9c85d45b2f871e37f31616b8
OhWanho <puppy1070@gmail.com>
2023-07-06 17:29:23 +0900

    오류복원

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ def div_func(n1,n2):
     return n1/n2
 def com_func(n1,n2):
     return n1**n2
-=======
-
->>>>>>> parent of 6e243f6 (곱하기 추가)
 
 
 ##전역변수부
@@ -38,5 +35,4 @@ result=com_func(num1,num2)
 print(num1,'^',num2,'=',result)
 
 
-=======
->>>>>>> parent of 6e243f6 (곱하기 추가)
+
