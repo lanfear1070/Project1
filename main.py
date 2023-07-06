934bbf89a2e58d09cc97de48b0c3d6453f839e52
OhWanho <puppy1070@gmail.com>
2023-07-06 14:47:47 +0900

    뺄셈함수 추가

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 ## 함수부
 def add_func(n1,n2):
     return n1+n2
+def sub_func(n1,n2):
+    return n1-n2
 
 
 
 ##전역변수부
-num1,num2,sum = 100,200,0
+num1,num2,result = 100,200,0
 
 
 ##메인코드
-sum=add_func(num1,num2)
-print(num1,'+',num2,'=',sum)
+result=add_func(num1,num2)
+print(num1,'+',num2,'=',result)
+
+result=sub_func(num1,num2)
+print(num1,'-',num2,'=',result)
